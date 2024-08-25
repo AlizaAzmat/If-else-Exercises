86590c56753868ef26315b56091f9b23f0996483
AlizaAzmat <itxmealizay@gmail.com>
2024-08-25 17:30:57 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,180 @@
+
+# =====================================================Exercise 2 ===================================================
+
+# Take a character input and check if character is C then print “C#”  otherwise print Invalid Character
+# Character 
+
+# Ex 1 
+# char = input("Please enter a character")
+# if char == "C":
+#     print("C#")
+# else:
+#     print("Invalid Character")
+
+
+# Take a number input and check if number is 7 Print “Seven” otherwise print “Invalid Number”. 
+#Number
+
+#Ex 2
+# num = int(input("Please enter a number"))
+# if num == 7:
+#     print("Seven")
+# else:
+#     print("Invalid Number")
+
+# Take a string input  and check if value is “one” then print 1 otherwise Print “Invalid String”
+#String
+
+# Ex 3 
+# strng= input("Please enter a string")
+# if strng == "one":
+#     print(1)
+# else:
+#     print("Invalid String")
+
+
+# Take Boolean value input and check if value is true then Print “Married” otherwise print “UnMarried” 
+# Boolean 
+# Ex 4
+# boolval = bool(input("Enter a boolean value"))
+# if boolval == True:
+#     print("Married")
+
+# else:
+#     print("Un-Married")
+
+
+# Take a floating value input and check if value is 13.19 then print “Correct Value” otherwise print “Invalid Value”
+# Float
+
+# Ex 5 
+# floatval = float(input("Please enter a floating value"))
+# if floatval ==  13.19:
+#     print("Correct Value")
+# else:
+#     print("Invalid Value")
+
+
+#  Take Price Input and and check if Price is 100000 then  Print “You can Buy Dell Laptop”  Otherwise Print “Invalid Amount”
+
+# Ex 6
+
+# price = int(input("Please enter a price"))
+# if price == 100000:
+#     print("You can Buy Dell Laptop")
+# else:
+#     print("Invalid Amount")
+
+
+# =====================================================Exercise 3 ===================================================
+
+# Take month number  input  and check if number is 2 then print February  otherwise Print “Unknown Number” 
+#Number
+
+# Ex 1
+
+# month_no = int(input("Please enter a Month"))
+# if month_no == 2:
+#     print("February")
+# else:
+#     print("Invalid Month")
+
+
+# Take month name input and check if value is “January” then print 1 otherwise print “Invalid Month” 
+# String 
+
+# Ex 2
+
+# month_name = input("Please enter a month name")
+# if month_name == "February":
+#     print(2)
+# else:
+#     print("Invalid Month")
+
+
+#  Take a Boolean value input and check if value is false then print “Variable Value is False” otherwise “ print “Variable value is True “ 
+# bool 
+
+# Ex 3
+
+# boolval = bool(input("Please enter a boolean value"))
+# if boolval == True:
+#     print("Variable Value is False")
+# else:
+#     print("Variable value is True")
+
+
+
+# Take a character input and check if character is „S‟ then print “Sindh”  otherwise print “Invalid Character”
+#character
+
+# Ex 4 
+
+# ch = input("Please enter a Province Character")
+# if ch == 'S':
+#     print("Sindh")
+# else:
+#     print("Invalid Character")
+
+
+
+# =====================================================Exercise 4 ===================================================
+
+# Take courseName  input  and check if value is “C#” then print  “C# Fee is 10000” otherwise Print “Invalid Course”. 
+# string 
+
+# Ex 1 
+
+# courseName = input ("Please enter a course name")
+# if courseName == "C#":
+#     print("C# Fee is 10000")
+# else:
+#     print("Invalid Course")
+
+
+
+
+# Take Fee input and Check if value is 10000 then print “You Can Enroll in C#” otherwise “Sorry You Can‟t Enroll in C#”.
+# Number 
+
+# Ex 2 
+
+# fee = int(input("Please enter a value "))
+# if fee == 10000:
+#     print("You Can Enroll in C#")
+# else:
+#     print("Sorry You Can‟t Enroll in C#")
+
+
+# Take a string input and Check if value is “Nokia” then print  
+# Model’s Available  
+# Lumia 535 
+# Lumia 635 
+# Lumia 720 
+# Otherwise Print “Unknown Company Name”.
+# String  
+
+# Ex 3 
+
+# modelstr = input("Please enter a string")
+# if modelstr == "Nokia":
+#     print("Model’s Available \n Lumia 535 \n Lumia 635 \n Lumia 720 ")
+# else:
+#     print("Unknown Company Name")
+
+
+# Take a string input and check if value is “Unary Operator” then print - - - - - - 
+# Increment Operator 
+# Decrement Operator 
+# Cast Operator  
+# Size of Operator 
+# Plus Operator 
+# Minus Operaor 
+# Otherwise print “Invalid Operator”.
+
+
+# operator = input("Please enter a operator ")
+# if operator == "Unary Operator":
+#     print(" Increment Operator \n Decrement Operator \n Cast Operator  \n Size of Operator \n Plus Operator \n Minus Operaor ")
+# else:
+#     print("Invalid Operator")
